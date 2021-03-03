4d3f2f882bf606d25cfca9a18fa51648fa4c35da
becky <beckymccarthy777@gmail.com>
2021-03-03 09:38:17 -0800

    3 in 1 stack

---
+++ 3_in_1_stack.js
@@ -0,0 +1,59 @@
+const stackCapacity = 5
+class MultiStacks {
+  constructor(stackCapacity) {
+  // find stack capacity - length of array/3  15/3 = 5 - index if .length 
+  this._stackCapacity = stackCapacity; // 5
+  // number of stacks  3
+  this.numOfStacks = 3  // 0, 1, 2
+  // array of the values - which is everything 0-4 stack
+  this.values = [];  // if it's full ifFull boolean
+  // index of top - how many are in it 
+  this.sizes = [0, 0, 0]
+  // & if is full is true or not
+  }  get stackCapacity() {
+    return this._stackCapacity;
+  }
+  indexOfTop(stackNo) {  }
+  isFull(stackNo) {
+    if (this.sizes[stackNo] === this.stackCapacity) {
+      return True
+    } else {
+      return False
+    }
+  }
+  // peek push and pop functionality
+  peek(stackNo) {
+    return 
+  }};let stack = new MultiStack(5);
+//console.log(stack.stackCapacity); // 5
+// console.log(stack.indexOfTop(0));
+// console.log(stack.values);
+// stack.push(0, 4); // "Item 4 has been successfully added to stack 0"
+// console.log(stack.indexOfTop(0));
+// console.log(stack.values);
+// stack.push(0, 3); // "Item 3 has been successfully added to stack 0"
+// console.log(stack.indexOfTop(0));
+// console.log(stack.values);
+// stack.push(0, 2); // ...
+// console.log(stack.indexOfTop(0));
+// console.log(stack.values);
+// stack.push(0, 1);
+// console.log(stack.indexOfTop(0));
+// console.log(stack.values);// stack.push(1, 8);
+// stack.push(1, 7);
+// stack.push(1, 6);
+// stack.push(1, 5);
+// stack.push(2, 12);
+// stack.push(2, 11);
+// stack.push(2, 10);
+// stack.push(2, 9); // "Item 9 has been successfully added to stack 2"// console.log(stack);
+// console.log(stack.peek(0)); // 1
+// console.log(stack.peek(1)); // 5
+// console.log(stack.peek(2)); // 9
+// console.log(stack.push(0, 16));
+// console.log(stack.peek(0)); // 16
+// console.log(stack.isFull(0)); // true
+// console.log(stack.push(0, 55)); // "Stack number 0 is full"
+// console.log(stack.pop(0)); // 16
+// console.log(stack.isFull(0)); // false
+// console.log(stack);
